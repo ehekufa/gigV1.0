dc1092e874eb80ba2806a7b46b702553a9289685
ehekufa <newcatroidfive5@gmail.com>
2026-07-28 15:34:51 +0300

    Update builtins.hpp

--- include/gig/builtins.hpp
+++ include/gig/builtins.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include "value.hpp"
-#include "environment.hpp"
 #include <vector>
 #include <iostream>
 
 namespace gig {
 
-// Объявления встроенных функций
+class Environment;   // forward declaration
+
 Value print_func(Environment& env, const std::vector<Value>& args);
 Value collectgarbage_func(Environment& env, const std::vector<Value>& args);
 Value type_func(Environment& env, const std::vector<Value>& args);
